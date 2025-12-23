9ceb065b70e85d956269feb2304ba614b2e14408
Nurul Taha <nurultaha.takaful@gmail.com>
2025-12-24 03:43:25 +0800

    Complete controller functions - show, update, destroy

--- app/Http/Controllers/API/productController.php
+++ app/Http/Controllers/API/productController.php
@@ -8,18 +8,16 @@ use Illuminate\Http\Request;
 
 class productController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    //Display a listing of the resource.
+
     public function index()
     {
         //return json output
         return response()->json(products::all());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    //Store a newly created resource in storage.
+     
     public function store(Request $request)
     {
         //validate request
@@ -29,29 +27,41 @@ class productController extends Controller
         'price' => 'required|numeric',
         'stock' => 'integer',
         ]);
+
+        //store product
+        $products = products::create($validated);
+
+        return response () -> json($products, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    // Display the specified resource.
+    
+    public function show(products $products)
     {
-        //
+        return response() -> json($products);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    //Update the specified resource in storage.
+    
+    public function update(Request $request, products $products)
     {
-        //
+        $validated = $request -> validate ([
+            'name' => 'String|max:255',
+            'description' => 'nullable|string',
+            'price' => 'numeric',
+            'stock' => 'integer',
+        ]);
+
+        $products -> update($validated);
+
+        return response() -> json($products);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    // Remove the specified resource from storage.
+
+    public function destroy(products $products)
     {
-        //
+        $products -> delete();
+        return response() -> json(null, 204);
     }
 }
