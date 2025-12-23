44799ec51945d53e2b2ee9b71259ceed4445905e
Nurul Taha <nurultaha.takaful@gmail.com>
2025-12-24 07:25:54 +0800

    Modify API route

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductsController extends Controller
+{
+    //Display a listing of the resource.
+    public function index()
+    {
+        $products = Product::all();
+        return response() -> json($products, 200);
+    }
+
+    //Store a newly created resource in storage.
+    public function store(Request $request)
+    {
+        $validated = $request -> validate([
+        'name' => 'required|string|max: 255',
+        'description' => 'nullable|text',
+        'price' => 'required|numeric',
+        'stock' => 'integer',
+        ]);
+
+        $product = Product::create($validated);
+
+        return response()->json([
+            'success' => true,
+            'data' => $product
+        ], 201);
+    }
+
+    // Display the specified resource.
+    public function show(product $product)
+    {
+        return response() -> json($product);
+    }
+
+    //Update the specified resource in storage.
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $validated = $request -> validate ([
+            'name' => 'String|max:255',
+            'description' => 'nullable|text',
+            'price' => 'numeric',
+            'stock' => 'integer',
+        ]);
+
+        $product -> update($validated);
+
+        return response() -> json([
+            'success' => true,
+            'data' => $product
+        ], 200);
+    }
+
+    // Remove the specified resource from storage.
+    public function destroy(product $product)
+    {
+        $product -> delete();
+        return response() -> json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,9 @@
-use App\Http\Controllers\API\productController;
+<?php
 
-Route::apiResource('products', productController::class);
+use App\Http\Controllers\ProductsController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('products', [ProductsController::class, 'index']);
+Route::post('products', [ProductsController::class, 'store']);
+Route::put('products/{id}', [ProductsController::class, 'update']);
+Route::delete('products/{id}', [ProductsController::class, 'destroy']);
