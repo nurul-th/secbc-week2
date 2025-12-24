0c7525b91c46215ab35c2e68a9aca77da965fddd
Nurul Taha <nurultaha.takaful@gmail.com>
2025-12-24 20:39:21 +0800

    Update show public function

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -34,9 +34,17 @@ class ProductsController extends Controller
     }
 
     // Display the specified resource.
-    public function show(Product $product)
+    public function show($id)
     {
-        return response() -> json($product);
+    $product = Product::find($id);
+
+    if (!$product) {
+        return response()->json([
+            'message' => 'Product not found'
+        ], 404);
+    }
+
+    return response()->json($product, 200);
     }
 
     //Update the specified resource in storage.
